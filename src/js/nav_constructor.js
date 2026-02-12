cdf53d89a25540063cee426caad0e4c150137f9a
nilou <ytvannilou0192@gmail.com>
2026-02-12 13:41:32 +0800

    war is over
    thank the lord

--- src/js/nav_constructor.js
+++ src/js/nav_constructor.js
@@ -3,39 +3,65 @@ function capitalized(string) {
 }
 
 function construct_nav() {
+
     let nav = document.querySelector("nav");
-    let pages = ["dashboard", "profile", "reservations"];
 
-    let head = document.createElement("h2");
-    head.textContent = "Archer's Lab";
+    if (document.title == "Manage Reservations" ||
+        document.title == "List of Reservations" ||
+        document.title == "In-person: View Slots" ||
+        document.title == "Room Requests" || 
+        document.title == "Admin Profile")
+    {
 
-	head.style.fontSize = "20px";
-	head.style.fontWeight = "200";
-	head.style.marginBottom = "50px";
+        let head = document.createElement("a");
+        head.setAttribute("href", "profile_admin.html")
+        head.textContent = "Archer's Lab";
+        nav.appendChild(head);
 
-    nav.appendChild(head);
+        let profile_admin = document.createElement("a");
+        profile_admin.setAttribute("href", "profile_admin.html")
+        profile_admin.textContent = "Profile";
+        nav.appendChild(profile_admin)
 
-    pages.forEach(page => {
-        let link = document.createElement("a");
-        link.textContent = capitalized(page);
-        link.setAttribute("href", page + ".html");
-        nav.appendChild(link);
-    });
+        let manage = document.createElement("a");
+        manage.setAttribute("href", "manage_reservations.html")
+        manage.textContent = "Manage Reservations"
+        nav.appendChild(manage);
 
-    let request_room = document.createElement("a");
-    request_room.setAttribute("href", "request_room.html")
-    request_room.textContent = "Request Room";
-    nav.appendChild(request_room);
+        let logout = document.createElement("a");
+        logout.setAttribute("href", "both_login_register.html")
+        logout.textContent = "Log Out"
+        nav.appendChild(logout);
 
-    let manage = document.createElement("a");
-    manage.setAttribute("href", "manage_reservations.html")
-    manage.textContent = "Manage Reservations"
-    nav.appendChild(manage);
+    }
+    else 
+    {
 
-    let logout = document.createElement("a");
-    logout.setAttribute("href", "both_login_register.html")
-    logout.textContent = "Log Out"
-    nav.appendChild(logout);
+        let head = document.createElement("a");
+        head.textContent = "Archer's Lab";
+        head.setAttribute("href", "dashboard.html")
+
+        nav.appendChild(head);
+
+        let pages = ["dashboard", "profile", "reservations"];
+
+        pages.forEach(page => {
+            let link = document.createElement("a");
+            link.textContent = capitalized(page);
+            link.setAttribute("href", page + ".html");
+            nav.appendChild(link);
+        });
+
+        let request_room = document.createElement("a");
+        request_room.setAttribute("href", "request_room.html")
+        request_room.textContent = "Request Room";
+        nav.appendChild(request_room);
+
+        let logout = document.createElement("a");
+        logout.setAttribute("href", "both_login_register.html")
+        logout.textContent = "Log Out"
+        nav.appendChild(logout);
+    }
 
     console.log("[nav_constructor.js] Initialized navigation bar!")
 }
