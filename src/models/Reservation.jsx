458ca79fce9a0cc606195375122d709f153f316b
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-06 20:37:10 +0800

    feat: implemented reservation class

--- src/models/Reservation.jsx
+++ src/models/Reservation.jsx
@@ -1,5 +1,16 @@
-class reservation {
-	constructor() {
-
+export class Reservation {
+	/*
+		Parameters:
+		date: datetime
+		time: string
+		room: string // TODO: make a room class
+		seats: int[5]
+	*/
+	constructor(date, time, room, seats) {
+		this.date = date;
+		this.time = time;
+		this.room = room;
+		this.seats = seats;
+		this.status = "Upcoming"
 	}
 }
