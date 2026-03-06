0984575894f9b9625704cf423a6e5a7085672c8d
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-06 20:38:26 +0800

    feat: implemented reservation table component into ui, disabled profile editing for now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Clock } from "../components/Clock";
 import { initialStudent } from "../models/Student";
+import { ReservationTable } from "../components/ReservationTable";
 
 export function Home () {
     return (
@@ -10,85 +11,7 @@ export function Home () {
         </div>
         <h2 className="mt-12 font-black google text-4xl">Current Reservations:</h2>
         <div className="mt-4 pl-4 pr-4 rounded-2xl gray-67 shadow-lg">
-            <table className="table-auto w-full text-left">
-                <thead className="font-bold border-b border-gray-600">
-                    <tr>
-                        <th>
-                            Date
-                        </th>
-                        <th>
-                            Time
-                        </th>
-                        <th>
-                            Room
-                        </th>
-                        <th>
-                            Capacity
-                        </th>
-                        <th>
-                            Seats Reserved
-                        </th>
-                        <th>
-                            Status
-                        </th>
-                        <th>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="border-b border-gray-600">
-                        <td>2/16/2025</td>
-                        <td>6:00AM-7:00AM</td>
-                        <td>GK201</td>
-                        <td>30/41</td>
-                        <td>1 1, 1 2</td>
-                        <td>Cancelled</td>
-                        <td className="flex items-center gap-2">
-                            <button className="ml-auto flex items-center gap-2 text-red-400 hover:text-red-600 hover:scale-105 transition-all duration-200">
-                                Cancel
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    height="20px"
-                                    width="20px"
-                                    viewBox="0 -960 960 960"
-                                    className="flex-shrink-0"
-                                >
-                                    <path
-                                    d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"
-                                    fill="currentColor"
-                                    />
-                                </svg>
-                            </button>
-                        </td>
-                    </tr>
-                    
-                    <tr>
-                        <td>2/16/2025</td>
-                        <td>6:00AM-7:00AM</td>
-                        <td>GK201</td>
-                        <td>30/41</td>
-                        <td>1 1, 1 2</td>
-                        <td>Cancelled</td>
-                        <td className="flex items-center gap-2 ">
-                            <button className="ml-auto flex items-center gap-2 text-red-400 hover:text-red-600 hover:scale-105 transition-all duration-200">
-                                Cancel
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    height="20px"
-                                    width="20px"
-                                    viewBox="0 -960 960 960"
-                                    className="flex-shrink-0"
-                                >
-                                    <path
-                                    d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"
-                                    fill="currentColor"
-                                    />
-                                </svg>
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <ReservationTable/>
         </div>
     </div>);
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { initialStudent } from "../models/Student";
 import { PencilSvg } from "../components/PencilSvg";
+import { ReservationTable } from "../components/ReservationTable";
 
 const fields = [
   { label: "NAME", key: "name", editable: false, display: false},
@@ -27,66 +28,66 @@ export function StudentProfile() {
 						<h1>{student[field.key]}</h1>
 					</div>
 				):
-				(<div></div>)
+				null
 			))}
 		</div>
 	);
 }
 
-export function StudentForm({ student, handleToggle }) {
-	const [formData, setFormData] = useState({ ...student });
+// export function StudentForm({ student, handleToggle }) {
+// 	const [formData, setFormData] = useState({ ...student });
 
-	const handleSave = () => {
-		Object.keys(formData).forEach((key) => {
-			student[key] = formData[key];
-		});
-		// alert("Profile updated!");
-		handleToggle();
-	};
+// 	const handleSave = () => {
+// 		Object.keys(formData).forEach((key) => {
+// 			student[key] = formData[key];
+// 		});
+// 		// alert("Profile updated!");
+// 		handleToggle();
+// 	};
 
-  	return (
-		<div className="space-y-2">
-		{fields.map((field) => (
-			field.editable ? (
-				<div
-					key={field.key}
-					className="my-2 rounded-2xl gray-89 p-2 transition-transform transform hover:scale-103"
-				>
-				<h2 className="font-bold text-xs">{field.label}</h2>
-				<input 
-					type="text" 
-					value={formData[field.key]}
-					onChange={(e) => setFormData(prev => ({ ...prev, [field.key]: e.target.value }))} 
-					className="w-full border-b border-gray-700 gray-89 focus:outline-none focus:border-blue-800 text-gray-900"
-				/>
-				</div>
-			) : (
-				<div
-					key={field.key}
-					className="my-2 rounded-2xl gray-89 p-2 transition-transform transform hover:scale-103 cursor-not-allowed"
-				>
-				<h2 className="font-bold text-xs">{field.label}</h2>
-				<h1>{student[field.key]}</h1>
-				</div>
-			)
-		))}
-			<div className="flex gap-2 justify-center mt-4">
-					<button 
-					className="border p-2 rounded-xl flex items-center 
-					transition transform active:scale-95"
-					onClick={handleSave}>
-					Save
-				</button>
-				<button 
-					className="border p-2 rounded-xl flex items-center 
-					transition transform active:scale-95"
-					onClick={handleToggle}>
-					Discard
-				</button>
-			</div>
-		</div>
-  	);
-}
+//   	return (
+// 		<div className="space-y-2">
+// 		{fields.map((field) => (
+// 			field.editable ? (
+// 				<div
+// 					key={field.key}
+// 					className="my-2 rounded-2xl gray-89 p-2 transition-transform transform hover:scale-103"
+// 				>
+// 				<h2 className="font-bold text-xs">{field.label}</h2>
+// 				<input 
+// 					type="text" 
+// 					value={formData[field.key]}
+// 					onChange={(e) => setFormData(prev => ({ ...prev, [field.key]: e.target.value }))} 
+// 					className="w-full border-b border-gray-700 gray-89 focus:outline-none focus:border-blue-800 text-gray-900"
+// 				/>
+// 				</div>
+// 			) : (
+// 				<div
+// 					key={field.key}
+// 					className="my-2 rounded-2xl gray-89 p-2 transition-transform transform hover:scale-103 cursor-not-allowed"
+// 				>
+// 				<h2 className="font-bold text-xs">{field.label}</h2>
+// 				<h1>{student[field.key]}</h1>
+// 				</div>
+// 			)
+// 		))}
+// 			<div className="flex gap-2 justify-center mt-4">
+// 					<button 
+// 					className="border p-2 rounded-xl flex items-center 
+// 					transition transform active:scale-95"
+// 					onClick={handleSave}>
+// 					Save
+// 				</button>
+// 				<button 
+// 					className="border p-2 rounded-xl flex items-center 
+// 					transition transform active:scale-95"
+// 					onClick={handleToggle}>
+// 					Discard
+// 				</button>
+// 			</div>
+// 		</div>
+//   	);
+// }
 
 export function Profile() {
     const [showFirst, setShowFirst] = useState(true);
@@ -96,11 +97,10 @@ export function Profile() {
     };
     
     return(
-        <div className="m-5 flex">
-            {showFirst ? (
-            <div className="mx-auto rounded-2xl pb-2 gray-67 flex flex-col items-center w-90">
+        <div className="m-5 flex gap-4">
+            <div className="rounded-2xl pb-2 gray-67 flex flex-col items-center w-1/3">
                 <img className="mt-10 mb-5 rounded-full w-40" src="./src/resources/karl.png"></img>
-                <h1 className="text-3xl mb-6 google font-bold">Karl Deejay Omandac</h1>
+                <h1 className="text-3xl mb-6 google font-bold">{initialStudent.name}</h1>
                 <div className="text-center mb-6">
                     <h2 className="font-[serif] italic text-xl">{initialStudent.bio}</h2>
                 </div>
@@ -111,27 +111,24 @@ export function Profile() {
                     className="border p-2 rounded-xl flex items-center 
                     transition transform active:scale-95 "
                     onClick={handleToggle}>
-                    <PencilSvg /> Edit Profile
+                    {showFirst ? (
+						<>
+							<PencilSvg /> Edit Profile
+						</>
+						
+					) : (
+						<>
+							<PencilSvg /> Cancel Editing
+						</>
+					)}
                 </button>
             </div>
-            ) : (
-            <div className="mx-auto rounded-2xl pb-2 gray-67 flex flex-col items-center w-90">
-                <img className="mt-10 mb-5 rounded-full w-40" src="./src/resources/karl.png"></img>
-                <button
-                    className="
-                    relative -mt-10 mr-30
-                    gray-89 text-white
-                    px-2 py-1 text-sm rounded flex items-center space-x-1
-                    ">
-                    <PencilSvg />Edit
-                </button>		
-                <div className="w-full mt-4 px-2 google">
-                    <StudentForm student={initialStudent} handleToggle={handleToggle} />
-                </div>
-            </div>
-
-            )}
-
+			<div className="rounded-2xl pb-2 gray-67 flex flex-col items-center w-2/3">
+				<div className="text-3xl mb-6 google font-bold mt-4">
+					Reservations
+				</div>
+				<ReservationTable/>
+			</div>
         </div>
     );
 }
