e76cc31807721e409fef6fcd3073c91440089e64
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-07 15:20:10 +0800

    feat: added default nickname for debugging, added isAdmin

--- src/models/Student.jsx
+++ src/models/Student.jsx
@@ -11,9 +11,9 @@ export class Student {
     course: string
     about: string
   */
-  constructor(name, id, bio, email, college, program, about) {
+  constructor(name, id, bio, email, college, program, about, isAdmin) {
     this.name = name;
-    this.nickname = "";
+    this.nickname = "Karl";
     this.id = id;
     this.bio = bio;
     this.email = email;
@@ -25,6 +25,7 @@ export class Student {
       new Reservation(new Date("2025-2-16"), "6:00AM-7:00AM", "GK201", [11, 12], "Upcoming"),
       new Reservation(new Date("2025-2-16"), "6:00AM-7:00AM", "GK201", [11, 12], "Cancelled"),
     ];
+    this.isAdmin = isAdmin;
   }
 }
 
@@ -35,5 +36,6 @@ export const initialStudent = new Student(
   "karl_omandac@dlsu.edu.ph",
   "College of Computer Studies",
   "Bachelor of Science in Computer Science",
-  "Epstein's most prized possession"
+  "Epstein's most prized possession",
+  0
 );
