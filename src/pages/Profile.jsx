a79e70aa46de9357cc47c92a9ebe16f2701696f6
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-07 15:21:07 +0800

    feat: implemented new StudentForm to be consistent with the old version, added SecurityForm (todo)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,11 @@ import { PencilSvg } from "../components/PencilSvg";
 import { ReservationTable } from "../components/ReservationTable";
 
 const fields = [
-  { label: "NAME", key: "name", editable: false, display: false},
+  { label: "NAME", key: "name", editable: false, display: true},
   { label: "ID", key: "id", editable: false, display: true},
-  { label: "BIO", key: "bio", editable: true, display: false},
   { label: "EMAIL", key: "email", editable: true, display: true},
   { label: "COLLEGE", key: "college", editable: true, display: true},
-  { label: "COURSE", key: "course", editable: true, display: true},
+  { label: "PROGRAM", key: "program", editable: true, display: true},
   { label: "ABOUT", key: "about", editable: true, display: true},
 ];
 
@@ -34,60 +33,96 @@ export function StudentProfile() {
 	);
 }
 
-// export function StudentForm({ student, handleToggle }) {
-// 	const [formData, setFormData] = useState({ ...student });
+export function StudentForm() {
+	return (
+		<form className="w-full px-4">
+			<div className="flex gap-[15px]">
+				<div className="mb-3 w-full flex-1">
+					<label className="block font-bold text-xs mb-1">Name</label>
+					<input 
+					className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+					type="text" value={initialStudent.name}></input>
+				</div>
+				<div className="mb-3 w-full flex-1">
+					<label className="block font-bold text-xs mb-1">Nickname</label>
+					<input 
+					className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+					type="text" value={initialStudent.nickname}></input>
+				</div>
+			</div>
 
-// 	const handleSave = () => {
-// 		Object.keys(formData).forEach((key) => {
-// 			student[key] = formData[key];
-// 		});
-// 		// alert("Profile updated!");
-// 		handleToggle();
-// 	};
+			<div className="flex gap-[15px]">
+				<div className="mb-3 w-full flex-1">
+					<label className="block font-bold text-xs mb-1">Student ID</label>
+					<input 
+					className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+					type="text" value={initialStudent.id}></input>
+				</div>
+				<div className="mb-3 w-full flex-1">
+					<label className="block font-bold text-xs mb-1">Email</label>
+					<input 
+					className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+					type="text" value={initialStudent.email}></input>
+				</div>
+			</div>
 
-//   	return (
-// 		<div className="space-y-2">
-// 		{fields.map((field) => (
-// 			field.editable ? (
-// 				<div
-// 					key={field.key}
-// 					className="my-2 rounded-2xl gray-89 p-2 transition-transform transform hover:scale-103"
-// 				>
-// 				<h2 className="font-bold text-xs">{field.label}</h2>
-// 				<input 
-// 					type="text" 
-// 					value={formData[field.key]}
-// 					onChange={(e) => setFormData(prev => ({ ...prev, [field.key]: e.target.value }))} 
-// 					className="w-full border-b border-gray-700 gray-89 focus:outline-none focus:border-blue-800 text-gray-900"
-// 				/>
-// 				</div>
-// 			) : (
-// 				<div
-// 					key={field.key}
-// 					className="my-2 rounded-2xl gray-89 p-2 transition-transform transform hover:scale-103 cursor-not-allowed"
-// 				>
-// 				<h2 className="font-bold text-xs">{field.label}</h2>
-// 				<h1>{student[field.key]}</h1>
-// 				</div>
-// 			)
-// 		))}
-// 			<div className="flex gap-2 justify-center mt-4">
-// 					<button 
-// 					className="border p-2 rounded-xl flex items-center 
-// 					transition transform active:scale-95"
-// 					onClick={handleSave}>
-// 					Save
-// 				</button>
-// 				<button 
-// 					className="border p-2 rounded-xl flex items-center 
-// 					transition transform active:scale-95"
-// 					onClick={handleToggle}>
-// 					Discard
-// 				</button>
-// 			</div>
-// 		</div>
-//   	);
-// }
+			<div className="mb-3 w-full">
+				<label className="block font-bold text-xs mb-1">College</label>
+				<input 
+				className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+				type="text" value={initialStudent.college}></input>
+			</div>
+
+			<div className="mb-3 w-full">
+				<label className="block font-bold text-xs mb-1">Program</label>
+				<input 
+				className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+				type="text" value={initialStudent.program}></input>
+			</div>
+
+			<div className="mb-3 w-full">
+				<label className="block font-bold text-xs mb-1">Bio</label>
+				<input 
+				className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+				type="text" value={initialStudent.bio}></input>
+			</div>
+
+			<div className="mb-3 w-full">
+				<label className="block font-bold text-xs mb-1">Description</label>
+				<textarea rows="2" 
+				className="w-full px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border"
+				type="text" value={initialStudent.about}></textarea>
+			</div>
+
+			<div className="flex justify-center mt-4">
+				<button 
+					className=" px-[10px] py-[6px] bg-[#C5C5C5] text-black
+					 rounded-md flex justify-center items-center
+					 active:scale-95 transition-transform transform hover:scale-103">
+					Save Changes
+				</button>
+			</div>
+		</form>
+	)
+}
+
+export function SecurityForm() {
+	return (
+		<div className="flex gap-5 w-full m-5 px-4">
+			<div className="w-1/2 px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border">
+				<div className="text-xlgoogle font-bold">
+					Change Password
+				</div>
+			</div>
+
+			<div className="w-1/2 px-[10px] py-[6px] border-2 border-[#ccc] rounded-md text-sm font-['Inter',sans-serif] box-border">
+				<div className="text-xlgoogle font-bold">
+					Delete Account
+				</div>
+			</div>
+		</div>
+	)
+}
 
 export function Profile() {
     const [showFirst, setShowFirst] = useState(true);
@@ -100,7 +135,9 @@ export function Profile() {
         <div className="m-5 flex gap-4">
             <div className="rounded-2xl pb-2 gray-67 flex flex-col items-center w-1/3">
                 <img className="mt-10 mb-5 rounded-full w-40" src="./src/resources/karl.png"></img>
-                <h1 className="text-3xl mb-6 google font-bold">{initialStudent.name}</h1>
+                <h1 className="text-3xl mb-6 google font-bold">{
+					initialStudent.nickname.length == 0 ? initialStudent.name : initialStudent.nickname
+				}</h1>
                 <div className="text-center mb-6">
                     <h2 className="font-[serif] italic text-xl">{initialStudent.bio}</h2>
                 </div>
@@ -108,7 +145,7 @@ export function Profile() {
                     <StudentProfile />
                 </div>
                 <button 
-                    className="border p-2 rounded-xl flex items-center 
+                    className="border p-2 rounded-xl flex items-center transition-transform transform hover:scale-103
                     transition transform active:scale-95 "
                     onClick={handleToggle}>
                     {showFirst ? (
@@ -123,12 +160,29 @@ export function Profile() {
 					)}
                 </button>
             </div>
-			<div className="rounded-2xl pb-2 gray-67 flex flex-col items-center w-2/3">
-				<div className="text-3xl mb-6 google font-bold mt-4">
-					Reservations
+			{showFirst ? (
+				<div className="rounded-2xl pb-2 gray-67 flex flex-col items-center w-2/3">
+					<div className="text-3xl mb-6 google font-bold mt-4">
+						Reservations
+					</div>
+					<ReservationTable/>
 				</div>
-				<ReservationTable/>
-			</div>
+			) : (
+				<div className="flex flex-col w-2/3 gap-4">
+					<div className="rounded-2xl pb-2 gray-67 flex flex-col items-center">
+						<div className="text-3xl mb-6 google font-bold mt-4">
+							Edit Details
+						</div>
+						<StudentForm/>
+					</div>
+					<div className="rounded-2xl pb-2 gray-67 flex flex-col items-center">
+						<div className="text-3xl mb-6 google font-bold mt-4">
+							Account Security
+						</div>
+						<SecurityForm/>
+					</div>
+				</div>
+			)}
         </div>
     );
 }
