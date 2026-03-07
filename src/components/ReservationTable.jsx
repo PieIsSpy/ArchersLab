0853a2c5da138e70e487eab50e088461369e1c87
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-07 14:05:32 +0800

    feat: added conditional checking for cancel button

--- src/components/ReservationTable.jsx
+++ src/components/ReservationTable.jsx
@@ -10,21 +10,23 @@ function ListReservations({student}) {
             <td>{res.seats.join(", ")}</td>
             <td>{res.status}</td>
             <td className="flex items-center gap-2">
-                <button className="ml-auto flex items-center gap-2 text-red-400 hover:text-red-600 hover:scale-105 transition-all duration-200">
-                    Cancel
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="20px"
-                        width="20px"
-                        viewBox="0 -960 960 960"
-                        className="flex-shrink-0"
-                    >
-                        <path
-                        d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"
-                        fill="currentColor"
-                        />
-                    </svg>
-                </button>
+                {res.status === "Upcoming" ? (
+                    <button className="ml-auto flex items-center gap-2 text-red-400 hover:text-red-600 hover:scale-105 transition-all duration-200">
+                        Cancel
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="20px"
+                            width="20px"
+                            viewBox="0 -960 960 960"
+                            className="flex-shrink-0"
+                        >
+                            <path
+                            d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"
+                            fill="currentColor"
+                            />
+                        </svg>
+                    </button>
+                ) : null}
             </td>
         </tr>
     ))
