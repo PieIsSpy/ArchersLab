91d8ba40db46cc050ccb6e055e1eff650c7d67bb
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-09 14:30:23 +0800

    feat: renamed student class to user class, added user list

---
+++ src/models/User.jsx
@@ -0,0 +1,44 @@
+export class Student {
+  /*
+    Parameters:
+    name: string
+    id: int
+    bio: string
+    email: string
+    college: string
+    course: string
+    about: string
+  */
+  constructor(name, id, bio, email, college, program, about, isAdmin) {
+    this.name = name;
+    this.nickname = "Karl";
+    this.id = id;
+    this.bio = bio;
+    this.email = email;
+    this.college = college;
+    this.program = program;
+    this.about = about;
+    this.password = "password";
+    this.isAdmin = isAdmin;
+  }
+
+  equals(otherUser) {
+    if (!otherUser) return false;
+    return this.id === otherUser.id;
+  }
+}
+
+let users = [
+  new Student(
+    "Karl Omandac",
+    "06706789",
+    "I invoke the fifth amendment.",
+    "karl_omandac@dlsu.edu.ph",
+    "College of Computer Studies",
+    "Bachelor of Science in Computer Science",
+    "Epstein's most prized possession",
+    1
+  )
+]
+
+export let currentUser = users[0];
