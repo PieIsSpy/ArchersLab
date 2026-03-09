fc075babc694f196511a72b8dec3620040b0f899
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-09 14:32:31 +0800

    feat: implemented curUser filter

--- src/components/ReservationTable.jsx
+++ src/components/ReservationTable.jsx
@@ -1,34 +1,37 @@
-import { currentUser } from "../models/Student";
+import { currentUser } from "../models/User";
+import { reservations } from "../models/Reservation";
 
-function ListStudentReservations({student}) {
-    return student.reservations.map((res, index) => (
-        <tr key={`${res.date}-${res.time}`} className="border-t-2 border-gray-67">
-            <td>{res.date.toLocaleDateString()}</td>
-            <td>{res.time}</td>
-            <td>{res.room}</td>
-            <td>30/41</td>
-            <td>{res.seats.join(", ")}</td>
-            <td>{res.status}</td>
-            <td className="flex items-center gap-2">
-                {res.status === "Upcoming" ? (
-                    <button className="ml-auto flex items-center gap-2 text-red-400 hover:text-red-600 hover:scale-105 transition-all duration-200">
-                        Cancel
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="20px"
-                            width="20px"
-                            viewBox="0 -960 960 960"
-                            className="flex-shrink-0"
-                        >
-                            <path
-                            d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"
-                            fill="currentColor"
-                            />
-                        </svg>
-                    </button>
-                ) : null}
-            </td>
-        </tr>
+function ListStudentReservations({student, reservations}) {
+    return reservations.filter(
+        res => student.equals(res.student)
+    ).map((res, index) => (
+        <tr key={`${index}`} className="border-t-2 border-gray-67">
+        <td>{res.date.toLocaleDateString()}</td>
+        <td>{res.time}</td>
+        <td>{res.room}</td>
+        <td>30/41</td>
+        <td>{res.seats.join(", ")}</td>
+        <td>{res.status}</td>
+        <td className="flex items-center gap-2">
+            {res.status === "Upcoming" ? (
+                <button className="ml-auto flex items-center gap-2 text-red-400 hover:text-red-600 hover:scale-105 transition-all duration-200">
+                    Cancel
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="20px"
+                        width="20px"
+                        viewBox="0 -960 960 960"
+                        className="flex-shrink-0"
+                    >
+                        <path
+                        d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"
+                        fill="currentColor"
+                        />
+                    </svg>
+                </button>
+            ) : null}
+        </td>
+    </tr>
     ))
 }
 
@@ -60,7 +63,7 @@ export function StudentReservationTable() {
                 </tr>
             </thead>
             <tbody>
-                <ListStudentReservations student={currentUser}/>
+                <ListStudentReservations student={currentUser} reservations={reservations}/>
             </tbody>
         </table>
     )
