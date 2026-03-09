69548498e79328e16406befa713a890b95b2f60f
nilou <ytvannilou0192@gmail.com>
2026-03-09 17:21:13 +0800

    feat: cleaning code, dark calendar

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect} from "react";
 import DatePicker from "react-datepicker";
-import { NavLink } from "react-router-dom";
+import "react-datepicker/dist/react-datepicker.css";
+import "../dark-datepicker.css";
 
 const days = 14; // you can only book 14 days after
 const rooms = 10;
@@ -26,30 +27,38 @@ for (let i = 0; i < days; i++) {
 }
 
 export function Reservations(){
-	// TODO
-	// maximum 5 reservations only per room (make an error message) (partial)
-	// reserve button will turn it (idk what color) (done)
-	// make hard coded data 
-	// ensure that an error pops up if you clicked on a booked seat (partial)
-	// fix ui, everything must be centered add combobox for rooms (partial)
-	// 2d array of seats
-	// make sure that going back from profile to idk it doesnt reset the array
-	// make sure for each date it has a 2d array of seats so its now [day][room][seat]
-	// bawal sundays to book kasi walang pasok
-	// dont forget the time ig... [day][room][seat][timeslot]
-	// <reservation done>
+	/* TODO:
+	- maximum 5 reservations only per room (make an error message) (partial)
+	- reserve button will turn it (idk what color) (done)
+	- make hard coded data 
+	- ensure that an error pops up if you clicked on a booked seat (partial)
+	- fix ui, everything must be centered add combobox for rooms (partial)
+	- 2d array of seats
+	- make sure that going back from profile to idk it doesnt reset the array
+	- make sure for each date it has a 2d array of seats so its now [day][room][seat]
+	- bawal sundays to book kasi walang pasok
+	- dont forget the time ig... [day][room][seat][timeslot]
+	- <reservation done>
+	*/ 
+
+	// Initialize options for select menu later
 	const timeSlots = [
 		"07:30-09:00", "09:15-10:45", "11:00-12:30", "12:45-14:15", 
 		"14:30-16:00", "16:15-17:45", "18:00-19:30",
 	];
-	const room = ["GK201", "GK202", "GK203", "GK204", "GK205", "GK206", "GK207", "GK208", "GK209", "GK210"]
+	const room = [
+		"GK201", "GK202", "GK203", "GK204", "GK205",
+		"GK206", "GK207", "GK208", "GK209", "GK210"
+	];
+
+	// Set states for React
 	const [selectedDate, setSelectedDate] = useState(()=>{
 		const today = new Date();
 		return today;
 	});
 	const [timeValue, setTimeValue] = useState(timeSlots[0]);
 	const [roomValue, setRoomValue] = useState(room[0])
-	const row = document.createElement("tr");
+
 	const DateChange = (date) => {
 		setSelectedDate(date)
 	};
@@ -59,25 +68,77 @@ export function Reservations(){
 		displayState();
 	}, [selectedDate, roomValue, timeValue]);
 	
-	const mindate = () => new Date();
-	
 
-	const chosenSeatsList = []; //la mesa sa right
+	const selectedSeats = [];
 
-	const selectedSeats = []; //stores a number
-
-	const table = document.getElementById("chosenSeatsTable");
 	const today = new Date();
-	today.setHours(0, 0, 0, 0);
 	const selected = new Date(selectedDate);
-	selected.setHours(0, 0, 0, 0);
-	const dayIndex = Math.floor((selected - today) / (1000 * 60 * 60 * 24)); //subtracts current - reservation date
 
+	today.setHours(0, 0, 0, 0);
+	selected.setHours(0, 0, 0, 0);
+
+	// Set indexes
+	const dayIndex = Math.floor((selected - today) / (1000 * 60 * 60 * 24)); //subtracts current - reservation date
 	const roomIndex = room.indexOf(roomValue);
 	const timeIndex = timeSlots.indexOf(timeValue);
 
+	// Set minimum and maximum date (earliest and latest)
+	const minDate = () => new Date();
 	const maxdate = () => {
 		return today.setDate(today.getDate() + 13);
+	}
+
+	// Initialize computer seats array
+	const computerArray = []
+	for (let i = 1; i<=5; i++)
+	{
+		const computerRow = []
+		for (let j = 1; j<=9; j++)
+		{
+			const seatID = (i-1)*9+j   
+			computerRow.push(
+				<button
+					id={seatID} 
+					onClick={() => selectSeat(seatID)}
+					className={`
+						w-16 h-12 hover:bg-gray-500 flex flex-col items-center justify-center my-9 
+						${j % 9 === 0? "mr-0" : j % 3 === 0 ? "mr-10" : "mr-0"}
+						`}>
+					<h1 className="m-0 text-xs">{9*(i-1)+j}</h1>
+					<img src="./src/resources/computers.png" alt="computer" className="w-50 h-25 object-contain"/>
+				</button>
+			);
+		}
+		computerArray.push(
+			<div className={`flex justify-center`}>
+				{computerRow}
+			</div>
+		);
+	}
+
+	
+	const optionRoom = [];
+	for (let i = 0; i< room.length; i++){
+		optionRoom.push(
+			<option
+				key={i}
+				value={room[i]}
+			>
+				{room[i]}
+			</option>
+		);
+	}
+
+	const timeSlotOptions = [];
+	for (let i = 0; i<timeSlots.length;i++){
+		timeSlotOptions.push(
+			<option
+				key={i}
+				value={timeSlots[i]}
+			>
+				{timeSlots[i]}
+			</option>
+		)
 	}
 
 	function reserveSeat(timeValue, roomValue, selectedSeats){
@@ -96,32 +157,29 @@ export function Reservations(){
 			`
 		);
 	
-		refreshSeats(dayIndex, roomIndex, timeIndex);
-
-		selectedSeats.splice(0, selectedSeats.length);
-		if (table) table.innerHTML = "";
-		
+		refreshSeats();
 	}
 
 	function refreshSeats() {
 		const seatStatus = reservations[dayIndex][roomIndex][timeIndex];
-	
 
-		for (let seatID = 1; seatID <= seatStatus.length; seatID++) {
-        const element = document.getElementById(seatID);
-        if (!element) continue;
+		for (let seatID = 1; seatID <= seatStatus.length; seatID++) 
+		{
+			const element = document.getElementById(seatID);
+			if (!element) continue;
 
-        element.classList.remove("blue");
+			element.classList.remove("blue");
 
-        if (seatStatus[seatID-1]) element.classList.add("gray-booked");
-        else element.classList.remove("gray-booked");
-    }
+			if (seatStatus[seatID-1]) 
+				element.classList.add("gray-booked");
+			else 
+				element.classList.remove("gray-booked");
+    	}
 
-    const table = document.getElementById("chosenSeatsTable");
-    if (table) table.innerHTML = "";
-    selectedSeats.splice(0, selectedSeats.length);
-    chosenSeatsList.splice(0, chosenSeatsList.length);
-
+		const table = document.getElementById("chosenSeatsTable");
+		if (table)
+			table.innerHTML = "";
+		selectedSeats.splice(0, selectedSeats.length);
 	}
 
 	function displayState(){
@@ -151,7 +209,6 @@ export function Reservations(){
 			`
 		);
 	}
-	
 
 	function selectSeat(seatID){
 		const element = document.getElementById(seatID);
@@ -209,103 +266,44 @@ export function Reservations(){
 			if(existingRow) existingRow.remove();
 		}
 	}
-
-	const computerCol = []
-	for(let i = 1; i<=5; i++){
-		const computerRow = []
-		for(let j = 1; j<=9; j++){
-			const seatID = 9*(i-1)+j   
-			computerRow.push(
-				
-				<button
-					id={seatID} 
-					onClick={() => selectSeat(seatID)}
-					className={`
-						w-16 h-12 hover:bg-gray-500 flex flex-col items-center justify-center my-9 
-						${j % 3 === 0 ? "mr-10" : "mr-0"}
-						`}>
-					<h1 className="m-0 text-xs">{9*(i-1)+j}</h1>
-					<img src="./src/resources/computers.png" alt="computer" className="w-50 h-25 object-contain"/>
-				</button>
-
-			);
-		}
-		computerCol.push(
-			<div className={`flex justify-center`}>
-				{computerRow}
-			</div>
-		);
-	}
-
-	
-	const optionRoom = [];
-	for (let i = 0; i< room.length; i++){
-		optionRoom.push(
-			<option
-				key={i}
-				value={room[i]}
-				style={{ backgroundColor: "#e0e0e0", color: "#000" }}
-			>
-				{room[i]}
-			</option>
-		);
-	}
-
-	const timeSlotOptions = [];
-	for (let i = 0; i<timeSlots.length;i++){
-		timeSlotOptions.push(
-			<option
-				key={i}
-				value={timeSlots[i]}
-				style={{ backgroundColor: "#e0e0e0", color: "#000" }}
-			>
-				{timeSlots[i]}
-			</option>
-		)
-	}
 	
 	return(
-		<div className="flex flex-col justify-center items-center rounded-2xl gap-8">
-			<div className="text-4xl google mt-5">
-				Reserve a room: 					
+		<div className="flex flex-col justify-center items-center rounded-2xl gap-3">
+			<div className="text-4xl google mt-10 font-bold mr-239">
+				Reserve a seat:		
 			</div>
-			<div className="flex flex-row gap-8">
+			<div className="flex flex-row gap-4">
 				<div className="flex flex-row justify-center items-center rounded-2xl gap-8">
 					<div className="flex items-center justify-center  flex flex-col gap-4">
-						<div className="gray-67 justify-center items-center rounded-2xl text-2xl google flex items-center justify-center gap-8 px-4">
+						<div className="gray-67 justify-center items-center rounded-2xl text-2xl google flex items-center justify-center gap-8 w-[800px]">
 							<div className="gap-2 flex flex-row justify-center items-center">
-								<div className="text-2xl google flex items-center justify-center">
+								<div className="text-xl google flex items-center justify-center">
 									Date:
 								</div>
-								<div className="w-[150px] h-[100px] flex items-center justify-center">
+								<div className="text-xl w-[150px] h-[100px] flex items-center justify-center">
 									<DatePicker
-										className="gray-89 text-white w-full p-2 rounded-xl text-center"
+										className="gray-89 text-xl w-full p-3 rounded-lg text-center"
 										selected={selectedDate}
-										onChange={(date) => {
-											setSelectedDate(date);
-										}}
-										
-										minDate={mindate()}
+										onChange={(date) => setSelectedDate(date)}
+										minDate={minDate()}
 										maxDate={maxdate()}
 										dateFormat="MM/dd/yyyy"
-										>
-									</DatePicker>
+									/>
 								</div>
 							</div>
 
-							<label className="gap-3 flex flex-row justify-center items-center">
+							<label className="text-xl gap-3 flex flex-row justify-center items-center">
 								<div>
 									Room:  
 								</div>
 								
 								<select
-								className = "gray-89 text-white"
+								className = "text-xl gray-89 text-center"
 								style={{
 									width: "120px",
 									height: "50px",
 									borderRadius: "8px",
 									padding: "6px 10px",
-									fontSize: "22px",
 								}}
 								value={roomValue}
 								onChange={(e) => {
@@ -320,18 +318,17 @@ export function Reservations(){
 								</select>
 							</label>
 
-							<label className="gap-3 flex flex-row justify-center items-center">
+							<label className="text-xl gap-3 flex flex-row justify-center items-center">
 								<div>
 									Timeslot:  
 								</div>
 								<select
-								className = "gray-89 text-white"
+								className = "text-xl gray-89"
 								style={{
-									width: "180px",
+									width: "150px",
 									height: "50px",
 									borderRadius: "8px",
 									padding: "6px 10px",
-									fontSize: "22px",
 								}}
 								value={timeValue}
 								onChange={(e) => {
@@ -349,7 +346,7 @@ export function Reservations(){
 
 			
 						<div className="w-[800px] h-[600px] gray-67 rounded-2xl">
-							{computerCol}
+							{computerArray}
 						</div>
 					</div>
 				</div>
@@ -371,7 +368,7 @@ export function Reservations(){
 						
 						<span>
 							Timeslot<br></br>
-							<div className="font-normal">
+							<div className="font-normal w-30">
 								{timeSlots[timeIndex]}
 							</div>
 						</span>
@@ -380,7 +377,7 @@ export function Reservations(){
 
 						<span>
 							Date<br></br>
-							<div className="font-normal">
+							<div className="font-normal w-24">
 								{selectedDate.toLocaleDateString()}
 							</div>
 						</span>
