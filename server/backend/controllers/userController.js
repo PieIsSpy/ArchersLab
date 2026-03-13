c736ff21c2f65490488a9b735b117d455e5944aa
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-13 23:29:02 +0800

    feat: added postman routes and controller for user

---
+++ server/backend/controllers/userController.js
@@ -0,0 +1,31 @@
+// @desc    Get User
+// @route   GET /api/users
+// @access  Private
+const getUsers = (req, res) => {
+    res.status(200).json({message: 'Get User'})
+}
+
+// @desc    Create User
+// @route   POST /api/users
+// @access  Private
+const createUser = (req, res) => {
+    res.status(200).json({message: 'Create User'})
+}
+
+// @desc    Update User
+// @route   PUT /api/users/:id
+// @access  Private
+const updateUser = (req, res) => {
+    res.status(200).json({message: `Update User ${req.params.id}`})
+}
+
+// @desc    Delete User
+// @route   DELETE /api/users/:id
+// @access  Private
+const deleteUser = (req, res) => {
+    res.status(200).json({message: `Delete User ${req.params.id}`})
+}
+
+module.exports = {
+    getUsers, createUser, updateUser, deleteUser
+}
