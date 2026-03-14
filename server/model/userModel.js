73c68a5399cc4eca3249caba269898352c39e358
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-14 21:07:57 +0800

    feat: implemented user schema and user controller functions

---
+++ server/model/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add your name']
+    },
+    nickname: {
+        type: String,
+        required: [false],
+        default: ''
+    },
+    _id: {
+        type: Number,
+        required: [true, 'Please add your id number']
+    },
+    bio: {
+        type: String,
+        required: [false],
+        default: ''
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add your DLSU Email'],
+    },
+    college: {
+        type: String,
+        required: [true, 'Please add your College'],
+    },
+    program: {
+        type: String,
+        required: [true, 'Please add your Program'],
+    },
+    about: {
+        type: String,
+        required: [false],
+        default: ''
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add your Password'],
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
