0f99cb67a17a0ad6393d27194bc92dafaf0cfb9b
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-14 12:06:30 +0800

    feat: added async handler for the functions of user controller

--- server/backend/controllers/userController.js
+++ server/backend/controllers/userController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get User
 // @route   GET /api/users
 // @access  Private
-const getUsers = (req, res) => {
+const getUsers = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get User'})
-}
+})
 
 // @desc    Create User
 // @route   POST /api/users
 // @access  Private
-const createUser = (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Invalid Creation')
     }
     res.status(200).json({message: 'Create User'})
-}
+})
 
 // @desc    Update User
 // @route   PUT /api/users/:id
 // @access  Private
-const updateUser = (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update User ${req.params.id}`})
-}
+})
 
 // @desc    Delete User
 // @route   DELETE /api/users/:id
 // @access  Private
-const deleteUser = (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete User ${req.params.id}`})
-}
+})
 
 module.exports = {
     getUsers, createUser, updateUser, deleteUser
