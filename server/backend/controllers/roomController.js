4fd20bd9d3bfc18e9d365fe6d093ca3197956778
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-15 20:43:06 +0800

    feat: added room schema

---
+++ server/backend/controllers/roomController.js
@@ -0,0 +1,68 @@
+const asyncHandler = require('express-async-handler')
+
+const Room = require('../../model/roomModel')
+
+// @desc    Get Room
+// @route   GET /api/rooms
+// @access  Private
+const getRooms = asyncHandler(async (req, res) => {
+    const rooms = await Room.find()
+
+    res.status(200).json(rooms)
+})
+
+// @desc    Create Room
+// @route   POST /api/rooms
+// @access  Private
+const createRoom = asyncHandler(async (req, res) => {
+    const {name, row, col, layout} = req.body;
+
+    if (!name || !row || !col || !layout) {
+        res.status(400)
+        throw new Error('Invalid Creation')
+    }
+
+    const room = await Room.create({
+        _id: name,
+        row: row,
+        col: col,
+        layout: layout
+    })
+
+    res.status(200).json(room)
+})
+
+// @desc    Update Room
+// @route   PUT /api/rooms/:id
+// @access  Private
+const updateRoom = asyncHandler(async (req, res) => {
+    const room = await Room.findById(req.params.id);
+
+    if (!room) {
+        res.status(400);
+        throw new Error('Room not found');
+    }
+
+    const updatedRoom = await Room.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedRoom)
+})
+
+// @desc    Delete Room
+// @route   DELETE /api/rooms/:id
+// @access  Private
+const deleteRoom = asyncHandler(async (req, res) => {
+    const room = await Room.findById(req.params.id);
+
+    if (!room) {
+        res.status(400);
+        throw new Error('Room not found');
+    }
+
+    await room.deleteOne();
+    res.status(200).json({ id: req.params.id });
+})
+
+module.exports = {
+    getRooms, createRoom, updateRoom, deleteRoom
+}
---
+++ server/model/roomModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const roomSchema = mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    row: {
+        type: Number,
+        required: true
+    },
+    col: {
+        type: Number,
+        required: true
+    },
+    layout: {
+        type: Number,
+        required: true
+    },
+})
