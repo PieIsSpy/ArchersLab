b9a02fbc15cc81e9fe0d1bd0b74e9146395f1c42
John <john_william_go@dlsu.edu.ph>
2026-03-19 02:43:04 +0800

    made the reservations exactly like sa ITS

--- client/src/models/Room.jsx
+++ client/src/models/Room.jsx
@@ -13,14 +13,29 @@ export class Room {
 }
 
 export let rooms = [
-    new Room("GK201", 5, 9, 1),
-    new Room("GK202", 5, 9, 1),
-    new Room("GK203", 5, 6, 1),
-    new Room("GK204", 5, 6, 1),
-    new Room("GK205", 5, 6, 2),
-    new Room("GK206", 5, 6, 2),
-    new Room("GK207", 4, 6, 2),
-    new Room("GK208", 4, 6, 3),
-    new Room("GK209", 4, 6, 3),
-    new Room("GK210", 4, 6, 3)
-]
+    new Room("A1904", 5, 9, 1),
+    new Room("C314", 6, 6, 2),
+    new Room("G210", 5, 6, 2),
+    new Room("G211", 5, 6, 1),
+    new Room("G302A", 4, 6, 3),
+    new Room("G302B", 4, 6, 3),
+    new Room("G304A", 4, 6, 3),
+    new Room("G304B", 5, 9, 1),
+    new Room("G306A", 4, 6, 3),
+    new Room("G306B", 4, 6, 3),
+    new Room("G404A", 5, 6, 3),
+    new Room("G404B", 5, 6, 3),
+    new Room("J212", 5, 4, 4),
+    new Room("L212", 3, 6, 2),
+    new Room("L229", 6, 9, 1),
+    new Room("L320", 5, 8, 5),
+    new Room("L335", 6, 8, 5),
+    new Room("V103", 5, 6, 2),
+    new Room("V205", 4, 6, 3),
+    new Room("V206", 4, 6, 3),
+    new Room("V208A", 4, 6, 3),
+    new Room("V208B", 4, 6, 3),
+    new Room("V301", 4, 9, 1),
+    new Room("V310", 5, 6, 2),
+    new Room("Y602", 5, 9, 1)
+];
