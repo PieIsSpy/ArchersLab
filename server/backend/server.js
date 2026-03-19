18e8d9720431d7b92a80114a132f9a7951939b7a
PieIsSpy <karldeejayomandac@gmail.com>
2026-03-19 09:00:15 +0800

    build: added express-sessions in server packages

--- server/backend/server.js
+++ server/backend/server.js
@@ -2,6 +2,7 @@
 // dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware');
@@ -12,9 +13,22 @@ connectDB();
 
 const app = express();
 
+app.use(session({
+    secret: 'key that will sign cookie',
+    resave: false,
+    saveUninitialized: false
+}))
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    req.session.isAuth = true;
+    console.log(req.session);
+    console.log(req.session.id);
+    // res.send("hello sessions")
+})
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/rooms', require('./routes/roomRoutes'));
