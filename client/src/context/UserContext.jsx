596a154beaa3d086b2457be5496e3816bc226c16
Floranaras <carlosranara0@gmail.com>
2026-03-22 09:22:05 +0800

    refactor: update UserContext with full typed shape

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,7 +1,10 @@
 import { createContext } from "react";
 
 export const UserContext = createContext({
-    currentUser: null,
-    setUser: () => {},
-    isAdmin: false
+  currentUser: null,
+  setUser: (_user) => {},
+  setIsAuth: (_val) => {},
+  setAdmin: (_val) => {},
+  isAdmin: false,
+  loading: false,
 });
