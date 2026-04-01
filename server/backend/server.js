f2526165d4e157391db145d5b3e83b7f36df890c
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-01 21:44:23 +0800

    feat: replaced hardcoded frontend url to urls from .env files

--- server/backend/server.js
+++ server/backend/server.js
@@ -26,7 +26,7 @@ store.on('connected', () => console.log('Store Connected to Mongo'))
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
