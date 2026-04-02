e971d31f3830fa43cdb786759d4b0cfd94e4798c
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-02 15:26:01 +0800

    feat: backend should now trust render proxy

--- server/backend/server.js
+++ server/backend/server.js
@@ -33,6 +33,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.set('trust proxy', 1);
 app.use(session({
     secret: 'key that will sign cookie',
     resave: false,
