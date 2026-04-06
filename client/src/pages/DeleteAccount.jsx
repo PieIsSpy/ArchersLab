663a80462c9cd8e485f5e84cd816c9742258bcec
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-06 11:07:26 +0800

    feat: made form design more consistent with other forms

--- client/src/pages/DeleteAccount.jsx
+++ client/src/pages/DeleteAccount.jsx
@@ -1,7 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { useState } from "react";
+
+import { FormPassword, FormLayout } from "../components/Form";
+import { Button } from "../components/Input";
 
 import { deleteAccount } from "../services/userServices";
 
@@ -46,46 +49,47 @@ export function DeleteAccount() {
     }
 
     return (
-        <div className="m-5 flex gap-4 align-center justify-center">
-            <div className="rounded-2xl pb-2 gray-67 flex flex-col items-center w-9/12">
-                <div className="mx-7">
-                    <div className="text-2xl mb-6 google font-bold mt-4">
-                        Delete Account
+        <FormLayout
+            title="Delete Account"
+            subtitle="WARNING! This deletes your account profile and data. Any reservations done prior to deletion are cancelled when your account is deleted."
+            onSubmit={handleSubmit}
+            children={
+                <>
+                    <FormPassword 
+                        label="Password"
+                        type="password"
+                        name="password"
+                        value={form.password}
+                        onChange={handleChange}
+                        placeholder="Enter password"
+                    />
+
+                    <FormPassword
+                        label="Confirm Password"
+                        type="password"
+                        name="confirmPassword"
+                        value={form.confirmPassword}
+                        onChange={handleChange}
+                        placeholder="Confirm password"
+                    />
+
+                    <div className="flex gap-6">
+                        <Button
+                            type="submit"
+                            label="Delete Account"
+                            color='red'
+                        />
+
+                        <Link to ={`/Profile/${currentUser._id}`}>
+                            <Button
+                                type="button"
+                                label="Exit"
+                                color="gray"
+                            />
+                        </Link>
                     </div>
-                    WARNING! This deletes your account profile and data. Any reservations
-                    done prior to deletion are cancelled when your account is deleted.
-                </div>
-                <div className="flex flex-col items-center justify-center gap-[10px]">
-                    <form onSubmit={handleSubmit}> 
-                        <table className="border-separate border-spacing-0">
-                            <tbody>
-                                <tr>
-                                    <th align="left" className="py-3 font-bold">Password</th>
-                                    <td className="w-[150px] py-3">
-                                        <input name="password" onChange={handleChange} type="password" className="w-full rounded border border-gray-400 p-1 focus:outline-none focus:ring-2 focus:ring-[#006341]" />
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <th align="left" className="py-3 font-bold">Confirm Password</th>
-                                    <td className="w-[150px] py-3">
-                                        <input name="confirmPassword" onChange={handleChange} type="password" className="bg-gray-89 confirm-password w-full rounded border border-gray-400 p-1 focus:outline-none focus:ring-2 focus:ring-[#006341]" />
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <th colSpan="2" align="right" className="pt-4">
-                                        <button type="submit" 
-                                            className="mt-[3px] px-[6px] py-[3px] bg-[#C5C5C5] text-black
-                                            rounded-md flex justify-center items-center
-                                            active:scale-95 transition-transform transform hover:scale-103">Submit</button>
-                                    </th>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </form>
-                </div>
-            </div>
-        </div>
+                </>
+            }
+        />
     )
 }
